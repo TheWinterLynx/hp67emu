3adcc6bf260e8a7c1ccdf0b6c681cd05ac3b74df
TheWinterLynx <64207335+TheWinterLynx@users.noreply.github.com>
2026-09-12 18:08:52 +0200

    Add hp67emu executable entry point

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+mod app;
+mod hp67;
+mod panel;
+
+use eframe::egui;
+
+fn main() -> eframe::Result<()> {
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([528.0, 992.0])
+            .with_min_inner_size([330.0, 620.0]),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "HP-67 Emulator",
+        options,
+        Box::new(|cc| Box::new(app::Hp67App::new(cc))),
+    )
+}
