f6a5231fb2bebab1600d6bf8f17e2141bdb5b9df
TheWinterLynx <64207335+TheWinterLynx@users.noreply.github.com>
2026-09-16 23:00:56 +0200

    Export ACT serial arithmetic coordinates

--- src/machines/hp67/mod.rs
+++ src/machines/hp67/mod.rs
@@ -23,6 +23,7 @@ pub use act::{
     ActOperation, ActRamImage, ActRegister, PowerOnActCore, PowerOnActError, PowerOnExecution,
     PowerOnOperation, ACT_RAM_WORDS, ACT_RETURN_STACK_DEPTH, ACT_STATUS_BITS, ACT_WORD_DIGITS,
 };
+pub use act_serial_execution::ActSerialArithmeticCoordinate;
 pub use act_serial_execution::ActSerialExecution;
 pub use act_serial_execution::ActSerialExecutionError;
 pub use act_serial_execution::ActSerialWordClass;
