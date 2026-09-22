5b01971ed65daaeb5e0fe27ec814d69d073fb068
TheWinterLynx <64207335+TheWinterLynx@users.noreply.github.com>
2026-09-22 11:01:06 +0200

    Export driver-bound HP-67 output capability

--- src/machines/hp67/mod.rs
+++ src/machines/hp67/mod.rs
@@ -67,7 +67,8 @@ pub use display_snapshot::{
     StructuralDisplaySlot,
 };
 pub use electrical::{
-    Hp67ElectricalError, Hp67ElectricalFabric, Hp67ElectricalSnapshot, Hp67ElectricalStager,
+    Hp67DriverStager, Hp67ElectricalError, Hp67ElectricalFabric, Hp67ElectricalSnapshot,
+    Hp67ElectricalStager,
 };
 pub use fetch::{
     run_structural_display_fetch_cycle, run_structural_fetch_cycle, ActSerialEndpoint,
